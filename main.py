b1af4553b820b484b0f1b510caa4a5fa9e19916f
LianxinGao <503773453@qq.com>
2025-12-28 21:49:14 +0800

    update

--- main.py
+++ main.py
@@ -3,10 +3,5 @@ from workflow.run_workflow import run_workflow
 import asyncio
 
 
-def run_single_inst():
-    # 运行所有币种的配置
-    return asyncio.run(run_workflow())
-
-
-trading_scheduler = TradingScheduler(agent_func=run_single_inst)
-trading_scheduler.start_with_immediate_execution()
+trading_scheduler = TradingScheduler(agent_func=run_workflow)
+asyncio.run(trading_scheduler.start_with_immediate_execution())
