9c3ea1f296fea3bf83f59672481d7ce5d0399e2f
LianxinGao <503773453@qq.com>
2025-12-30 18:10:16 +0800

    自动交易

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from schedule.trading_scheduler import TradingScheduler
 from workflow.run_workflow import run_workflow
 import asyncio
+from client import ok_client
+from client.ok_models import TdMode
+from coin_configs import coin_configs
 
+
+for config in coin_configs:
+    result = ok_client.set_leverage(config['inst_id'], "100", TdMode.CROSS)
+    print(f"设置合约杠杆为{config.LEVERAGE}倍: {result}")
 
 trading_scheduler = TradingScheduler(agent_func=run_workflow)
 asyncio.run(trading_scheduler.start_with_immediate_execution())
