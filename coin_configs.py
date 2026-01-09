aa0bc3a9622030222899a2f12f4966aca4c0d7ae
LianxinGao <503773453@qq.com>
2026-01-09 13:01:53 +0800

    更新

--- coin_configs.py
+++ coin_configs.py
@@ -1,11 +1,11 @@
 coin_configs = [
-    # {"inst_id": "BTC-USDT-SWAP", "intervals": [5, 60], "limit": 200, "precision": 2, 'sz': 0.1, 'leverage': '100'},
-    {"inst_id": "XAUT-USDT-SWAP", "intervals": [5, 60], "limit": 200, "precision": 2, 'sz': 5, 'leverage': '100'}
+    {"inst_id": "BTC-USDT-SWAP", "intervals": [5, 60], "limit": 200, "precision": 2, 'sz': 0.1, 'leverage': '100'},
+    # {"inst_id": "XAUT-USDT-SWAP", "intervals": [5, 60], "limit": 200, "precision": 2, 'sz': 5, 'leverage': '100'}
 ]
 
 monitoring_configs = [
-    # {"inst_id": "BTC-USDT-SWAP", "interval": 5, "limit": 200, "precision": 2},
-    {"inst_id": "XAUT-USDT-SWAP", "interval": 5, "limit": 200, "precision": 2},
+    {"inst_id": "BTC-USDT-SWAP", "interval": 5, "limit": 200, "precision": 2},
+    # {"inst_id": "XAUT-USDT-SWAP", "interval": 5, "limit": 200, "precision": 2},
 
 ]
 
