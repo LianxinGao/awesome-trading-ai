0fe966bdcc6075adab802483f156ec511d45ecb0
LianxinGao <503773453@qq.com>
2026-01-18 19:30:03 +0800

    update

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from workflow.run_workflow import run_workflow
 import asyncio
 
 
-trading_scheduler = TradingScheduler(agent_func=run_workflow, time_interval=5)
+trading_scheduler = TradingScheduler(agent_func=run_workflow, time_interval=15)
 asyncio.run(trading_scheduler.start_with_immediate_execution())
