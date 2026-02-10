32868293b20b77a4e9122f8cc9620a350e679676
LianxinGao <503773453@qq.com>
2026-02-10 21:26:14 +0800

    更新market cycle by ai

--- coin_configs.py
+++ coin_configs.py
@@ -1,7 +1,7 @@
 target_coins = [
     {"inst_id": "BTC-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 2, 'sz': 0.2, 'leverage': '100'},
-    {"inst_id": "ETH-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 2, 'sz': 0.5, 'leverage': '100'},
-    {"inst_id": "SOL-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 2, 'sz': 1, 'leverage': '50'},
+    # {"inst_id": "ETH-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 2, 'sz': 0.5, 'leverage': '100'},
+    # {"inst_id": "SOL-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 2, 'sz': 1, 'leverage': '50'},
     # {"inst_id": "XRP-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 4, 'sz': 1, 'leverage': '50'},
     # {"inst_id": "BNB-USDT-SWAP", "intervals": [15, 60], "limit": 200, "precision": 4, 'sz': 5, 'leverage': '50'},
 ]
